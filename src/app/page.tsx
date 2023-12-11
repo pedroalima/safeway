aee176e4f9daaebce77b0e9bcdf0f514b0459c50
Pedro Lima <tsc.pedrolima@gmail.com>
2023-12-10 22:41:38 -0300

    feat: add layout login prototype

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,34 @@
-import { url } from "inspector";
-
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World</h1>
+    <main className="min-w-full h-screen flex justify-center items-center">
+      <div className="-z-10 fixed w-full h-full">
+        <Image 
+          src="/bg-1.jpg"
+          layout="fill"
+          objectFit="cover"
+          alt="Background image"
+        />
+      </div>
+
+      <section className="backdrop-blur-sm bg-white/30 w-8/12 h-96 rounded p-8">
+        <h1>logo</h1>
+        <div className="h-full flex justify-between items-center">
+          <div>
+            <h2 className="text-6xl mb-8">Bem Vindo!</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          </div>
+          <form className="flex flex-col">
+            <h2 className="text-2xl">Login</h2>
+            <label htmlFor="user-name">Usuário</label>
+            <input type="text" name="user-name" id="user-name" />
+            <label htmlFor="password">Senha</label>
+            <input type="password" name="password" id="password" />
+            <input type="submit" value="Entrar" />
+          </form>
+        </div>
+      </section>
     </main>
   )
 }
