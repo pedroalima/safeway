9c1b477546fb6d3dbcd072446bdf83e75a98d597
Pedro Lima <tsc.pedrolima@gmail.com>
2023-12-11 23:17:29 -0300

    feat: add sign in component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,16 +16,20 @@ export default function Home() {
         <h1>logo</h1>
         <div className="h-full flex justify-between items-center">
           <div>
-            <h2 className="text-6xl mb-8">Bem Vindo!</h2>
+            <h2 className="text-6xl mb-8 font-bold">Bem Vindo!</h2>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
           </div>
-          <form className="flex flex-col">
-            <h2 className="text-2xl">Login</h2>
-            <label htmlFor="user-name">Usuário</label>
-            <input type="text" name="user-name" id="user-name" />
-            <label htmlFor="password">Senha</label>
-            <input type="password" name="password" id="password" />
-            <input type="submit" value="Entrar" />
+          <form className="flex flex-col bg-white p-8 rounded-3xl gap-6 mb-10 shadow-md">
+            <h2 className="text-2xl text-center mb-1 font-semibold">Login</h2>
+            <div className="flex flex-col gap-1">
+              <label htmlFor="user-name">Usuário</label>
+              <input className="bg-gray-100 rounded-full px-3 py-1" type="text" name="user-name" id="user-name" />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="password">Senha</label>
+              <input className="bg-gray-100 rounded-full px-3 py-1" type="password" name="password" id="password" />
+            </div>
+            <input className="bg-slate-800 text-slate-100 rounded-full p-2 mt-2" type="submit" value="Entrar" />
           </form>
         </div>
       </section>
