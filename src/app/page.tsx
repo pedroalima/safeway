5f916a3462dce66d67b6461558152592b091865d
Pedro Lima <tsc.pedrolima@gmail.com>
2023-12-12 22:33:39 -0300

    feat: add login desktop layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,14 +13,20 @@ export default function Home() {
       </div>
 
       <section className="backdrop-blur-sm bg-white/30 w-8/12 h-96 rounded p-8">
-        <h1>logo</h1>
+        <Image 
+          src="/logo.png"
+          width="60"
+          height="20"
+          alt="Logo"
+          className="rounded-full absolute top-5 left-5"
+        />
         <div className="h-full flex justify-between items-center">
           <div>
             <h2 className="text-6xl mb-8 font-bold">Bem Vindo!</h2>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
           </div>
-          <form className="flex flex-col bg-white p-8 rounded-3xl gap-6 mb-10 shadow-md">
-            <h2 className="text-2xl text-center mb-1 font-semibold">Login</h2>
+          <form className="flex flex-col bg-white p-8 rounded-3xl gap-5 shadow-md">
+            <h2 className="text-2xl text-center font-semibold">Login</h2>
             <div className="flex flex-col gap-1">
               <label htmlFor="user-name">Usuário</label>
               <input className="bg-gray-100 rounded-full px-3 py-1" type="text" name="user-name" id="user-name" />
@@ -29,7 +35,11 @@ export default function Home() {
               <label htmlFor="password">Senha</label>
               <input className="bg-gray-100 rounded-full px-3 py-1" type="password" name="password" id="password" />
             </div>
-            <input className="bg-slate-800 text-slate-100 rounded-full p-2 mt-2" type="submit" value="Entrar" />
+            <div className="flex justify-start gap-2">
+              <input type="checkbox" name="connected" id="connected" />
+              <label htmlFor="connected" className="text-xs">Manter conectado</label>
+            </div>
+            <input className="bg-slate-800 text-slate-100 rounded-full p-2" type="submit" value="Entrar" />
           </form>
         </div>
       </section>
