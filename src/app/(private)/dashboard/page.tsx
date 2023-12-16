721f8f4ebc7f587fed29bcff58cb8d106b9e0cc3
Pedro Lima <tsc.pedrolima@gmail.com>
2023-12-15 23:13:57 -0300

    style: add nav componenet in grid

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,4 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
+
+const navLinks = [
+    "Home",
+    "Dashboard",
+    "Projects",
+    "Tasks",
+    "Reporting",
+    "Users"
+]
 
 export default function Dashboard() {
   return (
@@ -13,27 +23,49 @@ export default function Dashboard() {
         </div>
 
         <div className="px-20 py-14 h-full grid grid-cols-8 grid-rows-6 gap-6">
-            <div className="bg-slate-100 col-start-1 col-end-3 row-start-1 row-end-7 rounded-2xl p-6">
-                <div className="flex justify-center items-center gap-4">
-                    <Image src="/logo.png" width="60" height="60" alt="Logo" className="rounded-full" />
+            <div className="bg-white col-start-1 col-end-3 row-start-1 row-end-7 rounded-2xl p-6 flex flex-col gap-8">
+                <div className="flex justify-start items-center gap-4">
+                    <Image 
+                        src="/logo.png" 
+                        width="45" 
+                        height="45" 
+                        alt="Logo" 
+                        className="rounded-full" 
+                    />
                     <span className="font-bold">SafeWay</span>
                 </div>
                 <div>
-                    <input type="search" name="search" id="search" placeholder="Search" className="rounded-lg w-full py-2 px-3" />
+                    <input type="search" name="search" id="search" placeholder="Search" className="bg-gray-100 rounded-lg w-full py-2 px-3" />
                 </div>
-                
-                <h1>Dashboard 1</h1>
+                <div className="px-3 flex flex-col gap-2">
+                    {navLinks.map((link, i) => (
+                        <Link href={`/${link}`} key={i} className="text-gray-400 hover:text-slate-950 hover:font-bold">{link}</Link>
+                    ))}
+                </div>
+                <div className="mt-auto flex justify-start items-center gap-3">
+                    <Image 
+                        src="/user.png"
+                        width="40"
+                        height="40"
+                        alt="Profile image"
+                    />
+                    <div className="flex flex-col">
+                        <span className="text-xs text-slate-950">Pedro Lima</span>
+                        <span className="text-xs text-gray-300">tsc.pedrolima@gmail.com</span>
+                    </div>
+                </div>
             </div>
-            <div className="bg-slate-100 col-start-3 col-end-9 row-start-1 row-end-2 rounded-2xl p-6">
+
+            <div className="bg-white col-start-3 col-end-9 row-start-1 row-end-2 rounded-2xl p-6">
                 <h1>Dashboard 2</h1>
             </div>
-            <div className="bg-slate-100 col-start-3 col-end-6 row-start-2 row-end-7 rounded-2xl p-6">
+            <div className="bg-white col-start-3 col-end-6 row-start-2 row-end-7 rounded-2xl p-6">
                 <h1>Dashboard 3</h1>
             </div>
-            <div className="bg-slate-100 col-start-6 col-end-9 row-start-2 row-end-5 rounded-2xl p-6">
+            <div className="bg-white col-start-6 col-end-9 row-start-2 row-end-5 rounded-2xl p-6">
                 <h1>Dashboard 4</h1>
             </div>
-            <div className="bg-slate-100 col-start-6 col-end-9 row-start-5 row-end-7 rounded-2xl p-6">
+            <div className="bg-white col-start-6 col-end-9 row-start-5 row-end-7 rounded-2xl p-6">
                 <h1>Dashboard 5</h1>
             </div>
         </div>
